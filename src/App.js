1bc913e86d9a1efbf1968b573f27a399d09abf1f
Felipe <felipe.psh@outlook.com>
2022-11-14 18:09:19 -0300

    finalizando ac

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form  action="/cadastrar" method="POST" name="cadastrar">
+          <label>Qual é seu nome?</label>
+          <input type={Text} name="nome"></input>
+      </form>
     </div>
   );
 }
